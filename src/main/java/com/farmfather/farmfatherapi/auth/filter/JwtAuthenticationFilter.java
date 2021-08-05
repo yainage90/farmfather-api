6b0e889897483d1d97d1fb37774c69b4a0c94ca6
yaincoding <yaincoding@gmail.com>
2021-08-05 23:52:58 +0900

    user is retrieved with id instead of email

--- src/main/java/com/farmfather/farmfatherapi/auth/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/farmfather/farmfatherapi/auth/filter/JwtAuthenticationFilter.java
@@ -1,16 +1,13 @@
 package com.farmfather.farmfatherapi.auth.filter;
 
 import java.io.IOException;
-
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import com.farmfather.farmfatherapi.auth.exception.JwtException;
 import com.farmfather.farmfatherapi.auth.service.JwtUserDetailsService;
-import com.farmfather.farmfatherapi.auth.service.JwtUtil;
-
+import com.farmfather.farmfatherapi.utils.JwtUtil;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -18,39 +15,44 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
-
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
 	private final JwtUserDetailsService jwtUserDetailsService;
 
-
-
 	@Override
-	public void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
+	public void doFilterInternal(HttpServletRequest request, HttpServletResponse response,
+			FilterChain chain) throws IOException, ServletException {
+
+		log.info(request.getPathInfo());
 
 		final String jwt = request.getHeader("jwt");
 
-		String email = null;
+		String id = null;
 
-		if(jwt != null) {
+		if (jwt != null) {
 			try {
-				email = JwtUtil.validate(jwt);
-				request.setAttribute("email", email);
-			} catch(JwtException e) {
+				id = JwtUtil.validate(jwt);
+				request.setAttribute("id", id);
+				log.info("jwt is valid. jwt=" + jwt);
+			} catch (JwtException e) {
+				log.error("authencation filter fail", e);
 				sendError(HttpStatus.UNAUTHORIZED, response);
 				return;
 			}
-		} 
+		}
 
-		if(email != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-			UserDetails userDetails = jwtUserDetailsService.loadUserByUsername(email);
-			UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = 
-			new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
-      usernamePasswordAuthenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails((HttpServletRequest)request));
+		if (id != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+			UserDetails userDetails = jwtUserDetailsService.getById(id);
+			UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken =
+					new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+			usernamePasswordAuthenticationToken.setDetails(
+					new WebAuthenticationDetailsSource().buildDetails((HttpServletRequest) request));
 
 			SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
 		}
@@ -61,7 +63,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
 	private void sendError(HttpStatus status, HttpServletResponse res) throws IOException {
 		res.setStatus(status.value());
-        res.setContentType("application/json");
+		res.setContentType("application/json");
 		res.sendError(status.value(), "UNAUTHORIZED");
 	}
 }
